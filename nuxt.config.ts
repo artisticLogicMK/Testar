32cdc97733f392d88d763def0458752f7b097d93
ArtisticLogic <mk.artisticlogic@gmail.com>
2022-12-17 15:27:28 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   modules: ['@nuxtjs/tailwindcss','nuxt-icon','@nuxtjs/supabase', '@vueuse/nuxt'],
   build: {
     //transpile: ['alpinejs']
