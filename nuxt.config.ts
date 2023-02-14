54c205371cddcbd77140d4b6586d5ab71596d01a
ArtisticLogic <mk.artisticlogic@gmail.com>
2023-02-14 10:25:57 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,8 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
         { name: 'description', content: 'Testar is an app for creating and publishing online tests and quizzes. With its user-friendly dashboard, professionals and hobbyists can quickly create engaging tests to assess knowledge and understanding. Testar makes it easy to share tests with others through a link, allowing them to participate and take the test. It\'s an excellent solution for anyone in need of a reliable and efficient way to create and administer online tests and quizzes.' },
         { name: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/learn-c73c3.appspot.com/o/Misc%2Ftester_social.png?alt=media&token=b2d6e5f6-f908-4501-beff-c3206e3036b4' },
-        { name: 'theme-color', content: '#138aa5' }
+        { name: 'theme-color', content: '#138aa5' },
+        { name: 'google-site-verification', content: '_vFj4hTmFULy15VgJ6YwoXFCqRJy-rcggvFXPPH_8lg' }
       ],
       script: [
         {src: 'https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js', defer: true},
